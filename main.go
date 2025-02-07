b708887e425f4d64b43ebb65abecd0d58353923a
riuzaki16 <riuzakii16@gmail.com>
2025-02-07 16:40:56 +0300

    pointers edit

--- main.go
+++ main.go
@@ -3,22 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	// a := [4]int{1, 2, 3, 4}
-	// reverse(&a)
-	// fmt.Println(a)
-	var a *int
-
-	fmt.Println(*a)
+	a := [4]int{1, 2, 3, 4}
+	reverse(&a)
+	fmt.Println(a)
 
 }
 
 
 
 
-// func reverse(arr *[4]int) {
-//   // for index, value := range *arr {
-// 	// 	arr[len(arr) - 1 - index] = value
-// 	// }
-// }
+func reverse(arr *[4]int) {
+  for index, value := range *arr {
+		arr[len(arr) - 1 - index] = value
+	}
+}
 
 
