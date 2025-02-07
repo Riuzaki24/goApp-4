2ea34365c1557bafd3fff2c5588f6ee07fc72c29
riuzaki16 <riuzakii16@gmail.com>
2025-02-07 16:39:12 +0300

    pointers

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	// a := [4]int{1, 2, 3, 4}
+	// reverse(&a)
+	// fmt.Println(a)
+	var a *int
+
+	fmt.Println(*a)
+
+}
+
+
+
+
+// func reverse(arr *[4]int) {
+//   // for index, value := range *arr {
+// 	// 	arr[len(arr) - 1 - index] = value
+// 	// }
+// }
+
+
