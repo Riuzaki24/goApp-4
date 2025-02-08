9a576176cbecf43d5e1aaa21ab7faca7f2faf621
riuzaki16 <riuzakii16@gmail.com>
2025-02-08 18:12:49 +0300

    struct mutation & func constructor

--- main.go
+++ main.go
@@ -11,23 +11,39 @@ type account struct {
 	url string
 }
 
+func (acc *account) outputPassword() {
+	fmt.Println(acc.login, acc.password, acc.url)
+}
+
+// Данная функция создает массив rune result, длины n, цикл for проходится по result, получая индекс(i) и записывая туда на каджый шаг символ letterRunes[rand.IntN(len(letterRunes))](берет случайный индекс из letterRunes)  
+func (acc *account) generatePassword(n int) {
+	result := make([]rune, n)
+	for i := range result {
+		result[i] = letterRunes[rand.IntN(len(letterRunes))]
+	} 
+	acc.password = string(result)
+}
+
+// Конструктор новой функции 
+func newAccount(login, passoword, url string) *account{
+	return &account{
+		url: url,
+		login: login,
+		password: password,
+	}
+}
+
+
 var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGJIKLMNOPQRSRUVWXYZ1234567890-*!")
 
 func main() {
-	fmt.Println(generatePassword(12))
-
-
 	login := promptData("Введите логин")
 	password := promptData("Введите пароль")
 	url := promptData("Введите URL")
-
-	myAccount := account{
-		password: password,
-		url: url,
-		login: login,
-	}
-
-	outputPassword(&myAccount)
+	myAccount := newAccount(login, password, url)
+	myAccount.generatePassword(12)
+	myAccount.outputPassword()
+	fmt.Println(myAccount)
 }
 
 
@@ -38,17 +54,3 @@ func promptData(prompt string) string {
 	return result
 }
 
-func outputPassword(acc *account) {
-	fmt.Println(acc.login, acc.password, acc.url)
-	fmt.Println(*acc)
-}
-
-
-// Данная функция создает массив rune result, длины n, цикл for проходится по result, получая индекс(i) и записывая туда на каджый шаг символ letterRunes[rand.IntN(len(letterRunes))](берет случайный индекс из letterRunes)  
-func generatePassword(n int) string {
-	result := make([]rune, n)
-	for i := range result {
-		result[i] = letterRunes[rand.IntN(len(letterRunes))]
-	} 
-	return string(result)
-}
